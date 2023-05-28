91c222bb268382cf8149260f5afa727044f32935
CherepanovVS <89967181+Che3108@users.noreply.github.com>
2023-05-28 20:42:48 +0800

    Update lib.py

--- server/lib.py
+++ server/lib.py
@@ -96,3 +96,312 @@ def main_magic(Facility_df, Russia_df):
     date_file(delta_date, max_date)
 
     print(*list(result.keys()), sep='\n', end='\n\n')
+    dict_max_kinosat = {}
+    dict_max_zorkii = {}
+
+    name_facility = Facility_df['Station'].unique()
+    satellit_name = Facility_df['Satellite'].unique()
+
+    result_df = pd.DataFrame(columns = ['Интервал', 'Длительность интервала', 'Спутник', 'Приемник',
+                                        'Заполненность спутника по завершении операции',
+                                        'Суммарный объем записанной информации',
+                                        'Суммарный объем сброшенной информации'])
+
+    # Индикатор занятости приемника
+    dict_block_dur = {}
+    for s in name_facility:
+        dict_block_dur[s] = pd.to_datetime('1900-01-01 00:00:00', format='%Y-%m-%d %H:%M:%S')
+    # Индикатор "занятости" спутника
+    dict_block_sat = {}
+    dict_fullness_sat = {}
+    for s in satellit_name:
+        dict_block_sat[s] = pd.to_datetime('1900-01-01 00:00:00', format='%Y-%m-%d %H:%M:%S')
+        dict_fullness_sat[s] = 0
+
+    full_sum_foto = 0
+    full_sum_save = 0
+    list_kinosat = ['_1101', '_1102', '_1103', '_1104', '_1105']
+
+    # Формирование таблиц интервалов видимости по станциям
+    for dates in list_date:
+        df = result[f"Facility_{dates}"]
+        res_df = pd.DataFrame(index=satellit_name, columns=name_facility)
+        for station in name_facility:
+            for sat in satellit_name:
+                df_cut = df.loc[df['Station'] == station]
+                df_cut = df_cut.loc[df_cut['Satellite'] == sat]
+                list_interval = []
+                for iter, rows in df_cut.iterrows():
+                    list_interval.append([rows['Start Time (UTCG)'], rows['Stop Time (UTCG)']])
+                res_df[station].loc[sat] = list_interval
+        result[f"Full_date_{dates}"] = res_df
+    print('Завершено формирование таблиц с интервалами видимости')
+
+    # Блок расчета возможного времени записи
+    for dates in list_date:
+        df = result[f"Russia_{dates}"]
+        res_df_1 = pd.DataFrame(index=satellit_name, columns=['for_foto'])
+        for sat in satellit_name:
+            df_cut = df.loc[df['Satellite'] == sat]
+            list_time_interval = []
+            list_duration = []
+            for iter, row in df_cut.iterrows():
+             for iter, row in df_cut.iterrows():
+                 list_time_interval.append([row['Start Time (UTCG)'],
+                                           row['Stop Time (UTCG)']])
+                list_duration.append(row['Duration (sec)'])
+            if len(list_time_interval) != 0:
+                for i in range(len(list_time_interval)):
+                    list_time_interval[i] = re.findall('\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}', str(list_time_interval[i]))
+                res_df_1['for_foto'].loc[sat] = list_time_interval
+            else:
+                res_df_1['for_foto'].loc[sat] = 0
+        result[f"For_foto_{dates}"] = res_df_1
+
+        print('Завершено формирование таблиц с интервалами съемки')
+        # Может пригодиться для "обрезания" списка съемки при заполнении нужного объема ЗУ
+        # На тестовых данных дало потерю 0,5 Тб
+        """
+        new_list_duration = []
+        if satellit[-7:-2] not in list_kinosat:
+            summa = 0
+            for dur in list_duration:
+                if summa < 0.21:
+                    new_list_duration.append(dur)
+                    summa  = summa + dur * 4 * 0.00012207
+                print(summa)
+            list_duration = new_list_duration
+        list_time_interval = list_time_interval[:len(list_duration)]
+        """
+        # Объединение съемки и зон видимости
+        sum_1 = result[f"Full_date_{dates}"]
+        sum_2 = result[f"For_foto_{dates}"]
+        full_data = pd.concat([sum_1, sum_2], axis=1)
+        result[f"Full_{dates}"] = full_data
+        print('Завершено формирование общей таблицы')
+
+        # Вычитание из зон видимости тех интервалов, которые пересекаются с интервалами записи
+        interval = result[f"Full_{dates}"]
+        for iter, row in interval.iterrows():
+            foto = re.findall('\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}', str(row['for_foto']))
+            foto_1 = foto[::2]
+            foto_2 = foto[1::2]
+            foto = []
+            for i in range(len(foto_1)):
+                foto.append([pd.to_datetime(foto_1[i], format='%Y-%m-%d %H:%M:%S'),
+                             pd.to_datetime(foto_2[i], format='%Y-%m-%d %H:%M:%S')])
+            for name in name_facility:
+                period = re.findall('\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}', str(row[name]))
+                period_1 = period[::2]
+                period_2 = period[1::2]
+                period = []
+                for i in range(len(period_1)):
+                    period.append([pd.to_datetime(period_1[i], format='%Y-%m-%d %H:%M:%S'),
+                                   pd.to_datetime(period_2[i], format='%Y-%m-%d %H:%M:%S')])
+                del_period = []
+                for j in period:
+                    for k in foto:
+                        date_on, date_off = intersection(j[0], j[1], k[0], k[1])
+                        if date_on != date_off:
+                            del_period.append(j)
+                new_period = [x for x in period if x not in del_period]
+
+                if len(new_period) != 0:
+                    for i in range(len(new_period)):
+                        new_period[i] = re.findall('\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}', str(new_period[i]))
+                row[name] = new_period
+        result[f"Full_without_foto_{dates}"] = interval
+        print('Закончен расчет исключения интервалов съемки из интервалов видимости')
+
+        # Расчет длительности каждого периода
+        def string_to_date(x):
+            x = re.findall('\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}', str(x))
+            x_1 = x[::2]
+            x_2 = x[1::2]
+            x = []
+            unique_date = []
+            for i in range(len(x_1)):
+                x.append([pd.to_datetime(x_1[i], format='%Y-%m-%d %H:%M:%S'),
+                          pd.to_datetime(x_2[i], format='%Y-%m-%d %H:%M:%S')])
+                date = x[i][0].date()
+                date_1 = x[i][1].date()
+                if date not in unique_date:
+                    unique_date.append(date)
+                if date_1 not in unique_date:
+                    unique_date.append(date_1)
+            return x, unique_date
+
+
+        timing = result[f"Full_without_foto_{dates}"]
+        #print(timing.columns)
+        timing_list = []
+        for iter, row in timing.iterrows():
+            for i in timing.columns:
+                time, unique_date = string_to_date(str(row[i]))
+                sum_time = 0
+                for k in time:
+                    sec = (k[1] - k[0]).total_seconds()
+                    sum_time = sum_time + sec
+                row[i] = sum_time
+            timing_list.append(row)
+        df_timing = pd.DataFrame(data=timing_list)
+        #print(df_timing)
+        result[f"Duration_{dates}"] = df_timing
+        print('Закончен расчет длительностей каждого интервала')
+
+        # Дополнительные столбцы для классификации спутников
+        duration = result[f"Duration_{dates}"]
+        print(duration['for_foto'])
+        duration['Суммарная видимость (минус интервалы записи)'] = 0
+        duration['Суммарная видимость (минус интервалы записи)'] = duration[name_facility].sum(axis=1)
+        # duration['Гигабайты, возможные для записи за интервал'] = duration['for_foto'] * 0.5
+
+        duration['Террабайты, возможные для записи за интервал'] = duration['for_foto'].astype(float) * 4 * 0.00012207
+        duration['Террабайты, возможные для записи в сутки (среднее)'] = duration[
+                                                                             'Террабайты, возможные для записи за интервал']
+
+        duration = duration.sort_values(by='for_foto')
+        duration['index_asc'] = 0
+        ind = 1
+        duration['satellit'] = 0
+        for iter, row in duration.iterrows():
+            if iter[-7:-2] in list_kinosat:
+                duration['satellit'].loc[iter] = 'Киносат'
+            else:
+                duration['satellit'].loc[iter] = 'Зоркий'
+            # duration['index_asc'].loc[duration.index == iter.map(str)] = ind
+            # ind = ind + 1
+
+        # duration['satellit'] = np.where(duration['index_asc'] <= 150, 'Зоркий', 'Киносат')
+        duration['volume'] = np.where(duration['satellit'] == 'Зоркий', 0.5, 1)
+        duration['Переполняемость за сутки'] = np.where(
+            duration['volume'] <= duration['Террабайты, возможные для записи за интервал'], 0, 1)
+        sum_kinosat = sum(duration['Террабайты, возможные для записи за интервал'].loc[duration['satellit'] == 'Киносат'])
+        time_kinosat_for_save = sum_kinosat * 8192
+        print(sum_kinosat, time_kinosat_for_save)
+        sum_zorkii = (24 * 60 * 60 * 14 - time_kinosat_for_save) * 0.25 * 0.00012207
+        time_zorkii = sum_zorkii * 8192
+        print(sum_zorkii, time_zorkii)
+        sum_zorkii_for_one = sum_zorkii / 150
+        time_zorkii_for_one = time_zorkii / 150
+        print(sum_zorkii_for_one, time_zorkii_for_one)
+
+        reserve = 5
+        max_sum_kinosat = sum_kinosat + 2 * reserve
+        max_sum_zorkii = sum_zorkii + reserve
+
+        dict_max_kinosat[dates] = max_sum_kinosat
+        dict_max_zorkii[dates] = max_sum_zorkii
+        result[f"Duration_added_{dates}"] = duration
+
+    print('Закончен расчет дополнительных столбцов длительности')
+    print(dict_max_kinosat)
+    print(dict_max_zorkii)
+    print('И вот теперь начинаем формировать расписание')
+    dict_mass = {}
+    count = 0
+    for dates in list_date:
+        df = result[f"Full_without_foto_{dates}"]
+        for iter, row in df.iterrows():
+            sat = iter
+            for col in df.columns:
+                if iter[-7:-2] in list_kinosat:
+                    type_sat = 1
+                else:
+                    type_sat = 0.5
+                receiving = col
+                save = re.findall('\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}', str(row[col]))
+                save_1 = save[::2]
+                save_2 = save[1::2]
+                for s in range(len(save_1)):
+                    time_interval = save_1[s] + '-' + save_2[s]
+                     #print(time_interval)
+                    dict_mass[count] = [sat, receiving, time_interval, (pd.to_datetime(save_2[s], format='%Y-%m-%d %H:%M:%S') -
+                                                                                 pd.to_datetime(save_1[s],
+                                                                                                format='%Y-%m-%d %H:%M:%S')).total_seconds(),
+                                        type_sat, [save_1[s], save_2[s]], dict_max_kinosat[dates], dict_max_zorkii[dates]]
+                    count = count + 1
+
+    dict_queue = {}
+    dict_block = {}
+    for x in dict_mass.keys():
+        interval_queue = pd.to_datetime(dict_mass[x][-3][0], format='%Y-%m-%d %H:%M:%S')
+        interval_block = pd.to_datetime(dict_mass[x][-3][1], format='%Y-%m-%d %H:%M:%S')
+        dict_queue[x] = interval_queue
+        dict_block[x] = interval_block
+
+    sorted_dict_queue = {}
+    sorted_keys = sorted(dict_queue, key=dict_queue.get)
+
+    for w in sorted_keys:
+        sorted_dict_queue[w] = dict_queue[w]
+    #print(sorted_dict_queue)
+    full_sum_zorkii = 0
+    full_sum_kinosat = 0
+    for zu in sorted_dict_queue.keys():
+        if dict_mass[zu][1] == 'for_foto':
+            if dict_block_sat[dict_mass[zu][0]] <= dict_queue[zu]:
+                # Проверяем, хватит ли у спутника места, чтобы фотографировать весь период
+                if dict_fullness_sat[dict_mass[zu][0]] + (dict_mass[zu][3] * 4 * gbit_to_tb) <= dict_mass[zu][4]:
+                    #print('Заполненность спутника до: ', dict_fullness_sat[dict_mass[zu][0]])
+                    if dict_mass[zu][4] == 1:
+                        if full_sum_zorkii + dict_mass[zu][3] * 4 * gbit_to_tb <= dict_mass[zu][-2]:
+                            dict_fullness_sat[dict_mass[zu][0]] = dict_fullness_sat[dict_mass[zu][0]] + dict_mass[zu][3] * 4 * gbit_to_tb
+                            dict_block_sat[dict_mass[zu][0]] = dict_block[zu]
+                            full_sum_foto = full_sum_foto + dict_mass[zu][3] * 4 * gbit_to_tb
+                            full_sum_zorkii = full_sum_zorkii + dict_mass[zu][3] * 4 * gbit_to_tb
+                    else:
+                         #if full_sum_kinosat + dict_mass[zu][3] * 4 * gbit_to_tb <= dict_mass[zu][-1]:
+                         dict_fullness_sat[dict_mass[zu][0]] = dict_fullness_sat[dict_mass[zu][0]] + dict_mass[zu][3] * 4 * gbit_to_tb
+                         dict_block_sat[dict_mass[zu][0]] = dict_block[zu]
+                         full_sum_foto = full_sum_foto + dict_mass[zu][3] * 4 * gbit_to_tb
+                         full_sum_kinosat = full_sum_kinosat + dict_mass[zu][3] * 4 * gbit_to_tb
+                         #print('Заполненность спутника после: ', dict_fullness_sat[dict_mass[zu][0]])
+                    new_row = pd.DataFrame([[dict_mass[zu][2],
+                                            dict_mass[zu][3],
+                                            dict_mass[zu][0],
+                                            dict_mass[zu][1],
+                                            dict_fullness_sat[dict_mass[zu][0]],
+                                            full_sum_foto,
+                                            full_sum_save]],
+                                           columns=['Интервал', 'Длительность интервала', 'Спутник',
+                                                    'Приемник', 'Заполненность спутника по завершении операции',
+                                                    'Суммарный объем записанной информации',
+                                                    'Суммарный объем сброшенной информации'])
+                    result_df = pd.concat([result_df, new_row], ignore_index=True)
+        else:
+            #print(dict_block_dur[dict_mass[zu][1]], dict_queue[zu])
+            if dict_block_dur[dict_mass[zu][1]] <= dict_queue[zu]:
+                #print(dict_block_sat[dict_mass[zu][0]], dict_queue[zu])
+                if dict_block_sat[dict_mass[zu][0]] <= dict_queue[zu]:
+                    if dict_mass[zu][4] == 1:
+                        vol_save = dict_mass[zu][3] * gbit_to_tb
+                    else:
+                        vol_save = dict_mass[zu][3] * 0.25 * gbit_to_tb
+                    #print('Объем сброса: ', vol_save)
+                    if dict_fullness_sat[dict_mass[zu][0]] <= dict_mass[zu][4] and dict_fullness_sat[dict_mass[zu][0]] > vol_save:
+                         full_sum_save = full_sum_save + vol_save
+                        #print('Заполненность спутника до: ', dict_fullness_sat[dict_mass[zu][0]])
+                        dict_fullness_sat[dict_mass[zu][0]] = dict_fullness_sat[dict_mass[zu][0]] - vol_save
+                        if dict_mass[zu][4] == 1:
+                            full_sum_kinosat = full_sum_kinosat - vol_save
+                        else:
+                            full_sum_zorkii = full_sum_zorkii - vol_save
+                        #print('Заполненность спутника после: ', dict_fullness_sat[dict_mass[zu][0]])
+                        dict_block_sat[dict_mass[zu][0]] = dict_block[zu]
+                        dict_block_dur[dict_mass[zu][1]] = dict_block[zu]
+                        new_row = pd.DataFrame([[dict_mass[zu][2],
+                                                 dict_mass[zu][3],
+                                                 dict_mass[zu][0],
+                                                 dict_mass[zu][1],
+                                                 dict_fullness_sat[dict_mass[zu][0]],
+                                                 full_sum_foto,
+                                                 full_sum_save]],
+                                                 columns=['Интервал', 'Длительность интервала', 'Спутник',
+                                                         'Приемник', 'Заполненность спутника по завершении операции',
+                                                         'Суммарный объем записанной информации',
+                                                         'Суммарный объем сброшенной информации'])
+                        result_df = pd.concat([result_df, new_row], ignore_index=True)
+    result["final_schdule"] = result_df
+    result = {k:v.to_dict('list') for k,v in result.items()}
+    return result
